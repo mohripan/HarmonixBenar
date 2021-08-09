ae0990c3064a5f2057ca554b4d77edc6812b2c42
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2021-08-09 19:48:38 +0700

    Fix error

--- Assets/Script/Environment/ChallangeGroundUp.cs
+++ Assets/Script/Environment/ChallangeGroundUp.cs
@@ -5,32 +5,24 @@ using UnityEngine;
 public class ChallangeGroundUp : MonoBehaviour
 {
     private bool valid;
-    private bool oneTime;
     [SerializeField] private PlayerSinging playerSinging;
     [SerializeField] private float speed;
-    [SerializeField] private Transform transform;
+    [SerializeField] private Transform transformer;
     [SerializeField] private float maxY;
-    [SerializeField] private GameObject singOrHumming;
 
     private Vector2 oldPosition;
 
     private void Start()
     {
-        SingHum(false);
-        oldPosition = transform.position;
+        oldPosition = transformer.position;
     }
 
-    private void SingHum(bool validasi)
-    {
-        if (singOrHumming != null)
-            singOrHumming.SetActive(validasi);
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            SingHum(!oneTime);
             valid = true;
         }
     }
@@ -39,7 +31,6 @@ public class ChallangeGroundUp : MonoBehaviour
     {
         if(collision.tag == "Player")
         {
-            SingHum(false);
             valid = false;
         }
     }
@@ -48,20 +39,15 @@ public class ChallangeGroundUp : MonoBehaviour
     {
         if (valid)
         {
-            if (playerSinging.valPitch > 0 && singOrHumming != null)
-            {
-                oneTime = true;
-                singOrHumming.SetActive(false);
-            }
             Physics2D.SyncTransforms();
             float yPosition = Mathf.Min(oldPosition.y + (playerSinging.valPitch / 35), oldPosition.y+maxY);
             Vector2 groundUp = new Vector2(oldPosition.x, yPosition);
 
-            transform.position = Vector2.MoveTowards(transform.position, groundUp, speed * Time.deltaTime);
+            transformer.position = Vector2.MoveTowards(transformer.position, groundUp, speed * Time.deltaTime);
         }
         else
         {
-            transform.position = Vector2.MoveTowards(transform.position, oldPosition, speed * Time.deltaTime);
+            transformer.position = Vector2.MoveTowards(transformer.position, oldPosition, speed * Time.deltaTime);
         }
     }
 }
